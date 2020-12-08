7d6399842581238a8d9e4f1ff2d5b04ccb1d5440
ingridt123 <ingridszenga@gmail.com>
2020-12-09 06:26:57 +0800

    Add day 8 for 2020 (refactor)

--- 2020/day8.py
+++ 2020/day8.py
@@ -2,6 +2,21 @@ from utils import readFile
 
 output = readFile("day8.txt", str, sepChar="\n")
 
+def executeInstruction(output, index, acc):
+    op = output[index][:3]
+    num = int(output[index][4:])
+    
+    if op == "acc":
+        acc += num
+        index += 1
+    elif op == "jmp":
+        index += num
+    else:
+        index += 1
+
+    return index, acc
+
+
 ### PART 1 ###
 
 acc1 = 0
@@ -9,21 +24,12 @@ index = 0
 visited = []
 
 while True:
-    op = output[index][:3]
-    num = int(output[index][4:])
-    
     if index in visited:
         break
     else:
         visited.append(index)
 
-    if op == "acc":
-        acc1 += num
-        index += 1
-    elif op == "jmp":
-        index += num
-    else:
-        index += 1
+    index, acc1 = executeInstruction(output, index, acc1)
 
 print(acc1)
 
@@ -47,25 +53,15 @@ for i in range(len(output)):
     visited = []
     infinite = False
     while True:
-        if index == len(output):
+        if index == len(output):        # reach instruction after last instruction
             break
-
-        op = output[index][:3]
-        num = int(output[index][4:])
-
-        if index in visited:
+        elif index in visited:          # visits an instruction again (loop)
             infinite = True
             break
         else:
             visited.append(index)
 
-        if op == "acc":
-            acc2 += num
-            index += 1
-        elif op == "jmp":
-            index += num
-        else:
-            index += 1
+        index, acc2 = executeInstruction(output, index, acc2)
 
     if infinite:
         output[i] = original
