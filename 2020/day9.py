3ab581d3c012a25c9460391b150528d7f997cb49
ingridt123 <ingridszenga@gmail.com>
2020-12-09 14:05:47 +0800

    Add day 9 for 2020

---
+++ 2020/day9.py
@@ -0,0 +1,51 @@
+from utils import readFile
+
+output = readFile("day9.txt", int)
+
+PREAMBLE_LENGTH = 25
+
+# check if two of previous preambleLength integers sum to output[i]
+def sumPrev(output, i, preambleLength):
+    sum = output[i]
+    numsDict = dict()
+    for k in range(i-preambleLength, i):
+        numsDict[output[k]] = sum - output[k]
+    
+    for n in numsDict:
+        if numsDict[n] in numsDict:
+            return True
+    
+    return False
+
+# check if group of two or more numbers sum to target
+def sumNext(output, i, target):
+    nums = []
+    while sum(nums) < target:
+        nums.append(output[i])
+        i += 1
+
+    if sum(nums) == target and len(nums) > 1:
+        return nums
+    else:
+        return []
+
+
+### PART 1 ###
+
+num = -1
+for i in range(PREAMBLE_LENGTH, len(output)):
+    if not sumPrev(output, i, PREAMBLE_LENGTH):
+        num = output[i]
+        break
+print(num)
+
+### PART 2 ###
+
+result = []
+for i in range(len(output)):
+    result = sumNext(output, i, num)
+    if result != []:
+        break
+min = min(result)
+max = max(result)
+print('%d + %d = %d' % (min, max, min+max))
